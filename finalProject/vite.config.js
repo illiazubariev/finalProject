ade1087bdc72b1d1db12d99c7e248282da3e433a
illiazubariev <zubarievillia@gmail.com>
2024-01-27 11:04:15 +0100

    add pages path

--- finalProject/vite.config.js
+++ finalProject/vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: "/finalProject",
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            nested: resolve(__dirname, 'nested/delivery.html'),
+          },
+        },
+      },
 });
 
 
