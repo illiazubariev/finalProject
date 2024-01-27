a1cf2edcb6c1888f8bfc2d49900f4cafe61642c3
illiazubariev <zubarievillia@gmail.com>
2024-01-27 10:52:50 +0100

    degrade fix path

--- finalProject/vite.config.js
+++ finalProject/vite.config.js
@@ -1,17 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-
-
-
 export default defineConfig({
-    build: {
-      base: "/finalProject",
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'index.html'),
-          nested: resolve(__dirname, 'nested/delivery.html'),
-        },
-      },
-    },
-  })
+    base: "/finalProject",
+});
+
+
+
